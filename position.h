17784dabbbe569c85d55b793492876d152ef05b8
yi7242 <47847960+yi7242@users.noreply.github.com>
2020-07-08 22:24:59 +0900

    nowTickとかspeedとかの単位を忘れてしましました

--- position.h
+++ position.h
@@ -5,25 +5,29 @@
 
 namespace Position {
 int speed;
-std::vector<note>& notes;
+std::vector<note> &notes; // type,time,lane,id
 int index;
 std::vector<note> drawing;
 std::vector<note> specials;
-
-void init(int _speed, std::vector<note>& _notes) {
-  notes = _notes;
-  speed = _speed;
-  index = 0;
-  drawing.clear();
-  specials.clear();
+int top_to_judge,balls; //上から判定まで届くのにかかる時間、上から判定までの球の数
+void init(int _speed, std::vector<note> &_notes) {
+    notes = _notes;
+    speed = _speed;
+    index = 0;
+    drawing.clear();
+    specials.clear();
+    top_to_judge = speed * balls; //計算式あってるか不確か
 }
 
 void update(unsigned long nowTick) {
-  updateDrawingList(nowTick);
-  Draw::draw(drawing, specials);
+    updateDrawingList(nowTick);
+    Draw::draw(drawing, specials);
 }
 void deleteNote(const int ID) {}
 void updateDrawingList(unsigned long nowTick) {
-  //伊藤
+    if(notes[index].time - top_to_judge >= nowTick) {
+        drawing.push_back(notes[index]);
+    }
+    //伊藤
 }
-}  // namespace Position
+} // namespace Position
